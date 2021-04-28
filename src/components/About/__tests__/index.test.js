e213cf1b73c8eb3999ef6f526b8a0c31e8dfb525
andrew <ap@ampiusa.com>
2021-04-28 11:54:48 -0700

    issue with images rendering

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,36 +1,19 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import About from '..';
+import React from 'react'
+import coverImage from "../../assets/cover/cover-image.jpg";
 
-afterEach(cleanup);
+function About() {
 
-describe('About component', () => {
-    // first Test
-    it('renders', () => {
-        render(<About />)       
-    });
-    
-    // Second test    
-    it('matches snapshot DOM node structure', () => {
-        const { asFragment } = render (<About />)
-        expect(asFragment()).toMatchSnapshot();
-        
-    })
-     
-})
+  return (
+    <section className="my-5" >
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+      </p>
+      </div>
+    </section>
+  )
+}
 
-describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-    const { getByLabelText } = render(<Nav />);
-  
-    expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
-  }) 
-
-  describe('links are visible', () => {
-    it('inserts text into the links', () => {
-      // Arrange
-      // Assert
-    });
-  })
+export default About
